083176baac4287eb08972c59e01b225ce1f8940b
Greg Babiars <gbabiars@Sarahs-iMac-10.local>
2014-12-22 10:08:03 -0700

    Added scripts to build
    Created initial component.  Added server and scripts to build.  Using tmp instead of dev for dev folder.

--- gulpfile.js
+++ gulpfile.js
@@ -11,3 +11,50 @@ gulp.task('html', function() {
     gulp.src('./app/index.html')
         .pipe(gulp.dest('./tmp'));
 });
+
+gulp.task('scripts', function() {
+    var bundler = browserify({
+        cache: {},
+        packagedCache: {},
+        fullPaths: true,
+        entries: ['./app/index.js'],
+        debug: true
+    });
+    bundler.transform(reactify);
+
+    function rebundle() {
+        console.log('Bundling scripts');
+        return bundler
+            .bundle()
+            .on('error', function(e) {
+                gutil.log('Browserify error', e);
+            })
+            .pipe(source('./app.js'))
+            .pipe(gulp.dest('./tmp'));
+    }
+
+    bundler = watchify(bundler);
+    bundler.on('update', rebundle);
+
+    return rebundle();
+});
+
+gulp.task('server', function() {
+    var server = new Hapi.Server();
+    server.connection({ port: 8000 });
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: {
+            directory: {
+                path: path.join(__dirname, '/tmp')
+            }
+        }
+    });
+
+    server.start(function() {
+        console.log('Dev server started', server.info.uri);
+    });
+});
+
+gulp.task('default', ['scripts', 'html', 'server']);
